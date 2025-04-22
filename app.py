73192620e5053660687ebfd4bf76491fe4d5570c
Caua-miguel <caua.sudol@outlook.com>
2025-04-22 13:27:59 -0300

    chore: downgrade modern config logging - py 3.12 to py 3.11

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import logging.config
-import logging.handlers
+import logging
 import pathlib
 import json
+import atexit
 
 def setup_logging():
     config_file = pathlib.Path("config.json")
     with open(config_file) as f_in:
         config = json.load(f_in)
     logging.config.dictConfig(config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
 
 def main():
     setup_logging()
